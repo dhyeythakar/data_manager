6784f9636409a9cf6c9785e96e9a35903a41b228
dhyeythakar <dhyeythakar87@gmail.com>
2026-06-28 12:20:27 +0530

    Add files via upload

---
+++ breakdemo.java
@@ -0,0 +1,18 @@
+//java program 1.19
+//java program to demonstrate the use of break statement
+//inside the for loop
+
+public class breakdemo {
+	public static void main(String[] args) {
+                //MAIN POINT THE KEYWORD CANNOT BE THE CLASS NAME 
+		//using for loop.
+		for(int a = 1; a <= 10; a++) {
+		if(a == 5) {
+			continue; //skip the no. 5
+			   } 
+
+			System.out.println(a);
+			
+				}	
+			}
+		}
